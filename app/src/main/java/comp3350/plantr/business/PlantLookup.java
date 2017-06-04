1296b568f3394f3133cf682e7aa640c612991721
KeatonMacLeod <macleodkeaton@gmail.com>
2017-06-04 15:07:24 -0500

    StubDatabase Revision: ------------------------ - Stub database now uses ArrayLists - Stub database uses a Singleton pattern - Revisions to the DatabaseInterface to model these changes

--- app/src/main/java/comp3350/plantr/business/PlantLookup.java
+++ app/src/main/java/comp3350/plantr/business/PlantLookup.java
@@ -4,28 +4,31 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
+import comp3350.plantr.objects.Plant;
 import comp3350.plantr.persistence.StubDatabase;
 
 /**
  * Created by Keaton MacLeod on 6/1/2017.
  */
 
-//This is the class used for the "Plant Lookup" page of our application.
-public class PlantLookup implements PlantLookupInterface {
-
-	public JSONObject getPlantByName(String plantName) throws JSONException {
-		StubDatabase database = new StubDatabase();
-		JSONArray plants = database.getAllTestPlants();
-		JSONObject plant = new JSONObject();
-		boolean foundPlant = false;
-		for (int a = 0; a < plants.length() && !foundPlant; a++)
-		{
-			if ((plants.getJSONObject(a).get("Name")).toString().toLowerCase().equals(plantName.toLowerCase()))
-			{
-				foundPlant = true;
-				plant = plants.getJSONObject(a);
-			}
-		}//for
-		return plant;
-	}//getPlantByName
-}//PlantLookup
+////This is the class used for the "Plant Lookup" page of our application.
+//public class PlantLookup implements PlantLookupInterface {
+//
+//	public JSONObject getPlantByName(String plantName) throws JSONException {
+//		StubDatabase database = new StubDatabase();
+//		ArrayList<Plant> plants = database.getAllPlants();
+//		Plant plant = new Plant();
+//		boolean foundPlant = false;
+//		for (int a = 0; a < plants.size() && !foundPlant; a++)
+//		{
+//			if ((plants.getJSONObject(a).get("Name")).toString().toLowerCase().equals(plantName.toLowerCase()))
+//			{
+//				foundPlant = true;
+//				plant = plants.getJSONObject(a);
+//			}
+//		}//for
+//		return plant;
+//	}//getPlantByName
+//}//PlantLookup
--- app/src/main/java/comp3350/plantr/persistence/StubDatabase.java
+++ app/src/main/java/comp3350/plantr/persistence/StubDatabase.java
@@ -1,47 +1,76 @@
 package comp3350.plantr.persistence;
 import comp3350.plantr.application.Constants;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONException;
-import java.util.Random;
+import comp3350.plantr.objects.Plant;
+import java.util.ArrayList;
+import comp3350.plantr.objects.Temperature;
 
 /**
  * Created by Keaton MacLeod on 5/30/2017.
  */
 
-public class StubDatabase implements StubDatabaseInterface{
+public class StubDatabase implements DatabaseInterface {
 
-    //Sample plant names / constants used for testing during iteration 0
-    Random RANDOM = new Random(); //Used to generated random test plants
-    final static private String[] PLANT_ARRAY = {Constants.ALOE,Constants.ANTHURIUM,Constants.ASPARAGUS_FERN,Constants.PEACE_LILY,
-                                                 Constants.PEPEROMIA,Constants.SNAKE_PLANT,Constants.DRACAENA,Constants.PHILODENDRON};
+    private static DatabaseInterface stubDatabaseInstance = null; //Used for Singleton
 
-    //Return a JSONObject representing single plant
-    public JSONObject getTestPlant() throws JSONException {
-        JSONObject plant = getRandomTestPlant();
+    //No Image, Temperature, Hardiness or Watering Frequency: Default Values Assigned Currently
+    private ArrayList<Plant> plants = new ArrayList<Plant>();
+
+    public StubDatabase() {
+        Plant aloe = new Plant(1, Constants.ALOE, Constants.PLANT_DESCRIPTOR + " " + Constants.ALOE , null, null, 1, 1);
+        Plant anthurium = new Plant(2, Constants.ANTHURIUM, Constants.PLANT_DESCRIPTOR + " " + Constants.ANTHURIUM , null, null, 1, 1);
+        Plant asparagusFern = new Plant(3, Constants.ASPARAGUS_FERN, Constants.PLANT_DESCRIPTOR + " " + Constants.ASPARAGUS_FERN , null, null, 1, 1);
+        Plant peaceLily = new Plant(4, Constants.PEACE_LILY, Constants.PLANT_DESCRIPTOR + " " + Constants.PEACE_LILY , null, null, 1, 1);
+        Plant peperomia = new Plant(5, Constants.PEPEROMIA, Constants.PLANT_DESCRIPTOR + " " + Constants.PEPEROMIA , null, null, 1, 1);
+        Plant snakePlant = new Plant(6, Constants.SNAKE_PLANT, Constants.PLANT_DESCRIPTOR + " " + Constants.SNAKE_PLANT , null, null, 1, 1);
+        Plant dracaena = new Plant(7, Constants.DRACAENA, Constants.PLANT_DESCRIPTOR + " " + Constants.DRACAENA , null, null, 1, 1);
+        Plant philodendron = new Plant(8, Constants.PHILODENDRON, Constants.PLANT_DESCRIPTOR + " " + Constants.PHILODENDRON , null, null, 1, 1);
+        plants.add(aloe);
+        plants.add(anthurium);
+        plants.add(asparagusFern);
+        plants.add(peaceLily);
+        plants.add(peperomia);
+        plants.add(snakePlant);
+        plants.add(dracaena);
+        plants.add(philodendron);
+    }//Constructor
+
+    //Singleton / Provide abstraction
+    @Override
+    public DatabaseInterface open() {
+        if (stubDatabaseInstance == null)
+        {
+            stubDatabaseInstance = new StubDatabase();
+        }
+        return stubDatabaseInstance;
+    }//open
+
+    //Return a Plant Object
+    @Override
+    public Plant getPlant(int id) {
+        Plant plant = null;
+        for (int a = 0; a < plants.size() && plant == null; a++)
+        {
+            if (plants.get(a).getPlantID() == id)
+                plant = plants.get(a);
+        }
         return plant;
     }//getPlant
 
-    //Return a JSONArray of all Test Plants
-    public JSONArray getAllTestPlants() throws JSONException {
-        JSONArray plants = new JSONArray();
-        for (int a = 0; a < PLANT_ARRAY.length; a++)
+    public Plant getPlant(String name)
+    {
+        Plant plant = null;
+        for (int a = 0; a < plants.size() && plant == null; a++)
         {
-			JSONObject plant = new JSONObject();
-            plant.put(Constants.NAME,PLANT_ARRAY[a]);
-            plant.put(Constants.DESCRIPTION,Constants.PLANT_DESCRIPTOR + " " + PLANT_ARRAY[a]);
-            plants.put(plant);
-        }//for
-        return plants;
-    }//getPlants
-
-    //Create a random JSONObject representing a plant
-    public JSONObject getRandomTestPlant() throws JSONException {
-        JSONObject plant = new JSONObject();
-        int plantNumber = RANDOM.nextInt(PLANT_ARRAY.length);
-        plant.put(Constants.NAME,PLANT_ARRAY[plantNumber]);
-        plant.put(Constants.DESCRIPTION,Constants.PLANT_DESCRIPTOR + " " + PLANT_ARRAY[plantNumber]);
+            if (plants.get(a).getPlantName().toLowerCase() == name.toLowerCase())
+                plant = plants.get(a);
+        }
         return plant;
-    }//createPlant
+    }//getPlant
+
+    //Return an ArrayList of all Plant Objects
+    @Override
+    public ArrayList<Plant> getAllPlants() {
+		return plants;
+    }//getAllPlants
 
 }//StubDatabase
