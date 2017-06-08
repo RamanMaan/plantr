ff1594c66bb5ac2b0800ea177354e8bca51f5c1a
Raman Maan <maanr@myumanitoba.ca>
2017-06-08 00:08:06 -0500

    Reorganizing files into packages, deleting temp files

--- app/src/test/java/AllTests.java
+++ app/src/test/java/AllTests.java
@@ -1,21 +1,22 @@
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import comp3350.plantr.tests.StubDatabaseTest;
+import comp3350.plantr.tests.persistence.StubDatabaseTest;
 import comp3350.plantr.tests.application.DatabaseAccessTest;
 import comp3350.plantr.tests.business.DifficultyTest;
+import comp3350.plantr.tests.objects.PlantTest;
 import comp3350.plantr.tests.objects.TemperatureTest;
 
 /**
  * 6/4/2017
  * Raman Maan
- *
+ * <p>
  * Purpose: Encompasses all test suite classes for easy run
  */
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-		TemperatureTest.class, StubDatabaseTest.class, DatabaseAccessTest.class, DifficultyTest.class
+		TemperatureTest.class, StubDatabaseTest.class, DatabaseAccessTest.class, DifficultyTest.class, PlantTest.class
 })
 public class AllTests {
 }
