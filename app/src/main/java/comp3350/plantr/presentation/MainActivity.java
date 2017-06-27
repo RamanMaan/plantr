d63636399a9b3f37adf18abb570988f4767166b4
Raman Maan <maanr@myumanitoba.ca>
2017-06-26 23:33:04 -0500

    Plantipedia now starts activity for plant ListView

--- app/src/main/java/comp3350/plantr/presentation/MainActivity.java
+++ app/src/main/java/comp3350/plantr/presentation/MainActivity.java
@@ -1,7 +1,6 @@
 package comp3350.plantr.presentation;
 
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,7 +8,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import comp3350.plantr.R;
--- app/src/main/java/comp3350/plantr/presentation/fragments/PlantipediaFragment.java
+++ app/src/main/java/comp3350/plantr/presentation/fragments/PlantipediaFragment.java
@@ -1,13 +1,16 @@
 package comp3350.plantr.presentation.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import comp3350.plantr.R;
+import comp3350.plantr.presentation.PlantListView;
 
 /**
  * Plantipedia view
@@ -20,6 +23,15 @@ public class PlantipediaFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 		myView = inflater.inflate(R.layout.plantipedia_layout, container, false);
+
+		Button btn = (Button) myView.findViewById(R.id.view_plants_button);
+		btn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getActivity(), PlantListView.class));
+			}
+		});
+
 		return myView;
 	}
 
