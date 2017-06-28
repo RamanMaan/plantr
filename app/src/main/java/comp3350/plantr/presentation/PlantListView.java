7516ced839612ca2b0c672849d8162b46bac72ef
KeatonMacLeod <macleodkeaton@gmail.com>
2017-06-27 22:43:37 -0500

    (TEMPORARY RESERVE WHILE I MERGE) Personal Plant View: ---------------------
    - Added more GUI elements to the page

--- app/src/main/java/comp3350/plantr/presentation/PlantListView.java
+++ app/src/main/java/comp3350/plantr/presentation/PlantListView.java
@@ -3,30 +3,24 @@ package comp3350.plantr.presentation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.INotificationSideChannel;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import comp3350.plantr.R;
 import comp3350.plantr.business.DatabaseAccess;
 import comp3350.plantr.model.Plant;
 import comp3350.plantr.persistence.DatabaseInterface;
-import comp3350.plantr.persistence.StubDatabase;
 
 /**
  * Created by KevinD on 6/5/2017.
  */
 
-public class PlantListView extends AppCompatActivity{
+public class PlantListView extends AppCompatActivity {
 
 	private static final String TAG = "PlantListView";
 
@@ -36,15 +30,16 @@ public class PlantListView extends AppCompatActivity{
 		DatabaseInterface db;
 
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_plant_list_view);
+		setContentView(R.layout.activity_plant_view);
 		Log.d(TAG, "onCreate: started.");
 
-		ListView list = (ListView) findViewById(R.id.plantList);
+		ListView list = (ListView) findViewById(R.id.plantipedia_listview);
 
 		//initialize the database
 		db = DatabaseAccess.open();
-		ArrayList<Plant> plantList = (ArrayList<Plant>) db.getAllPlants();
-		PlantListAdapter adapter = new PlantListAdapter(this, plantList);
+
+		List<Plant> plantList = db.getAllPlants();
+		PlantListAdapter adapter = new PlantListAdapter(this, 1, plantList);
 
 		list.setAdapter(adapter);
 		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -52,7 +47,7 @@ public class PlantListView extends AppCompatActivity{
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				Intent intent = new Intent(PlantListView.this, PlantView.class);
 				//store the plant ID with the intent to display
-				intent.putExtra(getString(R.string.plantID), position+1);
+				intent.putExtra(getString(R.string.plant_id), position);
 				startActivity(intent);
 			}
 		});
