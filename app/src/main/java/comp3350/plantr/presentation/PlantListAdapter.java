6e6d756f40fd130b92776c86c0dc5a9705eae358
Raman Maan <maanr@myumanitoba.ca>
2017-06-27 19:15:56 -0500

    DE57 - updated plantipedia layout to match our new design
    Removed old layouts and classes.
    Added new in fragment while also getting rid of splash page.
    Separating intent-id's into their own xml for organization
    
    Close #57

--- app/src/main/java/comp3350/plantr/presentation/PlantListAdapter.java
+++ app/src/main/java/comp3350/plantr/presentation/PlantListAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -18,8 +19,11 @@ import comp3350.plantr.model.Plant;
 
 public class PlantListAdapter extends ArrayAdapter<Plant> {
 
-	public PlantListAdapter(Context context, List<Plant> plants) {
-		super(context, 0, plants);
+	private Context context;
+
+	public PlantListAdapter(Context context, int resource, List<Plant> plants) {
+		super(context, resource, plants);
+		this.context = context;
 	}
 
 	@Override
@@ -31,11 +35,13 @@ public class PlantListAdapter extends ArrayAdapter<Plant> {
 			convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_plant_list_item, parent, false);
 		}
 		// Lookup view for data population
-		TextView plantName = (TextView) convertView.findViewById(R.id.plantListName);
+		TextView plantName = (TextView) convertView.findViewById(R.id.plantlist_text);
+		ImageView plantImg = (ImageView) convertView.findViewById(R.id.plantlist_icon);
 		// Populate the data into the template view using the data object
 		plantName.setText(plant.getPlantName());
-		// Return the completed view to render on screen
+		plantImg.setImageResource(context.getResources().getIdentifier("@drawable/" + plant.getPlantImg(), null, context.getPackageName()));
 
+		// Return the completed view to render on screen
 		return convertView;
 	}
 }
