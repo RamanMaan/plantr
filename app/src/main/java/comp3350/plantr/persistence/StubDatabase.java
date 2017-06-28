3d6f3d4ffc2ddc5ab1923db835f12f78a2f48b0c
MRamsay <jasperarlington@gmail.com>
2017-06-28 16:47:33 -0500

    DUS39 - Modify Users Garden Plants
    Added Garden to the stub database, and personal plant getters in the DatabaseInterface

--- app/src/main/java/comp3350/plantr/persistence/StubDatabase.java
+++ app/src/main/java/comp3350/plantr/persistence/StubDatabase.java
@@ -2,22 +2,28 @@ package comp3350.plantr.persistence;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
+import comp3350.plantr.model.Garden;
+import comp3350.plantr.model.PersonalPlant;
 import comp3350.plantr.model.Plant;
 import comp3350.plantr.model.Temperature;
 import comp3350.plantr.model.TemperatureRange;
 
 /**
  * Created by Keaton MacLeod on 5/30/2017.
+ *
+ * Stub Database, used to mimick the behavior of a real database
  */
 
 public class StubDatabase implements DatabaseInterface {
 
 	private ArrayList<Plant> plants;
+	private Garden _userGarden;
 
 	public StubDatabase() {
-		plants = new ArrayList<Plant>(Arrays.asList(
+		plants = new ArrayList<>(Arrays.asList(
 				new Plant(0, "Aloe", "Aloe", "aloe", new TemperatureRange(new Temperature(21), new Temperature(23)), 170),
 				new Plant(1, "Anthurium", "Anthurium", "anthurium", new TemperatureRange(new Temperature(21), new Temperature(23)), 50),
 				new Plant(2, "Asparagus fern", "Asparagus fern", "asparagus_fern", new TemperatureRange(new Temperature(21), new Temperature(30)), 1),
@@ -27,6 +33,8 @@ public class StubDatabase implements DatabaseInterface {
 				new Plant(6, "Dracaena", "Dracaena", "dracaena", new TemperatureRange(new Temperature(21), new Temperature(23)), 1),
 				new Plant(7, "Philodendron", "Philodendron", "philodendron", new TemperatureRange(new Temperature(21), new Temperature(23)), 1)
 		));
+
+		_userGarden = new Garden();
 
 	}//Constructor
 
@@ -68,5 +76,15 @@ public class StubDatabase implements DatabaseInterface {
 	public List<Plant> getAllPlants() {
 		return plants;
 	}//getAllPlants
+
+	@Override
+	public PersonalPlant getPersonalPlantByID(int ID){
+		return _userGarden.getPersonalPlantById(ID);
+	}
+
+	@Override
+	public Collection<PersonalPlant> getAllPersonalPlants(){
+		return _userGarden.getAllPlants();
+	}
 
 }//StubDatabase
