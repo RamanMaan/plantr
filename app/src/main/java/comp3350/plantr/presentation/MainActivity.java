8c58ba8ff202323e11dd2c04235004731c705b3a
Raman Maan <maanr@myumanitoba.ca>
2017-06-29 03:48:15 -0500

    Copying database to device

--- app/src/main/java/comp3350/plantr/presentation/MainActivity.java
+++ app/src/main/java/comp3350/plantr/presentation/MainActivity.java
@@ -1,6 +1,8 @@
 package comp3350.plantr.presentation;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,7 +12,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import comp3350.plantr.R;
+import comp3350.plantr.business.DatabaseAccess;
 
 public class MainActivity extends AppCompatActivity
 		implements NavigationView.OnNavigationItemSelectedListener {
@@ -18,6 +26,10 @@ public class MainActivity extends AppCompatActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		copyDatabaseToDevice();
+		DatabaseAccess.open();
+
 		setContentView(R.layout.activity_main);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
@@ -33,6 +45,13 @@ public class MainActivity extends AppCompatActivity
 
 		//set default fragment to garden layout
 		displayFragment(R.id.nav_garden_layout);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		DatabaseAccess.close();
 	}
 
 	@Override
@@ -72,5 +91,56 @@ public class MainActivity extends AppCompatActivity
 
 		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 		drawer.closeDrawer(GravityCompat.START);
+	}
+
+	private void copyDatabaseToDevice() {
+		final String DB_PATH = "db";
+
+		String[] assetNames;
+		Context context = getApplicationContext();
+		File dataDirectory = context.getDir(DB_PATH, Context.MODE_PRIVATE);
+		AssetManager assetManager = getAssets();
+
+		try {
+
+			assetNames = assetManager.list(DB_PATH);
+			for (int i = 0; i < assetNames.length; i++) {
+				assetNames[i] = DB_PATH + "/" + assetNames[i];
+			}
+
+			copyAssetsToDirectory(assetNames, dataDirectory);
+
+			DatabaseAccess.setDBPathName(dataDirectory.toString() + "/" + DatabaseAccess.dbName);
+
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	public void copyAssetsToDirectory(String[] assets, File directory) throws IOException {
+		AssetManager assetManager = getAssets();
+
+		for (String asset : assets) {
+			String[] components = asset.split("/");
+			String copyPath = directory.toString() + "/" + components[components.length - 1];
+			char[] buffer = new char[1024];
+			int count;
+
+			File outFile = new File(copyPath);
+
+			if (!outFile.exists()) {
+				InputStreamReader in = new InputStreamReader(assetManager.open(asset));
+				FileWriter out = new FileWriter(outFile);
+
+				count = in.read(buffer);
+				while (count != -1) {
+					out.write(buffer, 0, count);
+					count = in.read(buffer);
+				}
+
+				out.close();
+				in.close();
+			}
+		}
 	}
 }
