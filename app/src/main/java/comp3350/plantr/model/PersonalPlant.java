73da0874dac993fde57b598853e27a2457a4664d
Raman Maan <maanr@myumanitoba.ca>
2017-06-29 00:47:04 -0500

    US44 - Added ability to water plants
    Close issue #53

--- app/src/main/java/comp3350/plantr/model/PersonalPlant.java
+++ app/src/main/java/comp3350/plantr/model/PersonalPlant.java
@@ -1,55 +1,64 @@
 package comp3350.plantr.model;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
  * Created by Michael on 27/06/2017.
- *
+ * <p>
  * A specific instance of a plant.
  */
 
 public class PersonalPlant {
 
-	//private static int idCounter = 0;
-
 	private Plant _plantType;
 	private String _plantName;
 	private int _personalPlantID;
+	private Date _lastWatered;
 
-
-//	public PersonalPlant(Plant plantType, String plantName){
-//		_plantType = plantType;
-//		_plantName = plantName;
-//		_personalPlantID = idCounter++;
-//	}
-
-	public PersonalPlant(Plant plantType, String plantName, int id){
+	public PersonalPlant(Plant plantType, String plantName, int id) {
 		_plantType = plantType;
 		_plantName = plantName;
 		_personalPlantID = id;
-
+		_lastWatered = new Date();
 	}
 
-	public boolean equals(Object other){
+	public boolean equals(Object other) {
 		boolean isSame = false;
 
-		if(other instanceof PersonalPlant){ //comparing two PersonalPlants
+		if (other instanceof PersonalPlant) { //comparing two PersonalPlants
 			isSame = ((PersonalPlant) other)._personalPlantID == this._personalPlantID;
 		}
 
 		return isSame;
 	}
 
-	public boolean equals(int compareID){
+	public boolean equals(int compareID) {
 		return _personalPlantID == compareID;
 	}
 
-	public int getID(){
+	public int getID() {
 		return _personalPlantID;
 	}
 
 	public String getName() { return _plantName; }
 
 	public Plant getType() { return _plantType; }
+
+	public Date getLastWatered() {
+		return _lastWatered;
+	}
+
+	public void setLastWatered(Date d) {
+		_lastWatered = d;
+	}
+
+	public Date getNextWatering() {
+		Date nextWatering = _lastWatered;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(nextWatering);
+		cal.add(Calendar.HOUR_OF_DAY, _plantType.getWateringFreq());
+		return cal.getTime();
+	}
 
 }
