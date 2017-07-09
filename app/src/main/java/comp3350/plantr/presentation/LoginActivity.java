2330628d571452dd57b154e245990da61b574491
Raman Maan <maanr@myumanitoba.ca>
2017-07-09 17:07:28 -0500

    Database generated in login activity instead of main
    LoginActivity is now our initial activity so it should be responsible for database generation

--- app/src/main/java/comp3350/plantr/presentation/LoginActivity.java
+++ app/src/main/java/comp3350/plantr/presentation/LoginActivity.java
@@ -1,11 +1,18 @@
 package comp3350.plantr.presentation;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import comp3350.plantr.R;
 import comp3350.plantr.business.DatabaseAccess;
@@ -18,6 +25,17 @@ public class LoginActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		copyDatabaseToDevice();
+		try {
+			//This is where the database is switched between stub and regular
+			DatabaseAccess.open();
+			//DatabaseAccess.openStub();
+		} catch (DatabaseStartFailureException e) {
+			Toast.makeText(getApplicationContext(), R.string.login_database_init_failure, Toast.LENGTH_LONG).show();
+			e.printStackTrace();
+		}
+
 		setContentView(R.layout.activity_login);
 	}
 
@@ -43,6 +61,54 @@ public class LoginActivity extends AppCompatActivity {
 		} catch (UserLoginException e) {
 			Toast.makeText(getApplicationContext(), R.string.login_failedlogin, Toast.LENGTH_LONG).show();
 			e.printStackTrace();
+		}
+	}
+
+	private void copyDatabaseToDevice() {
+		final String DB_PATH = "db";
+
+		String[] assetNames;
+		Context context = getApplicationContext();
+		File dataDirectory = context.getDir(DB_PATH, Context.MODE_PRIVATE);
+		AssetManager assetManager = getAssets();
+
+		try {
+			assetNames = assetManager.list(DB_PATH);
+			for (int i = 0; i < assetNames.length; i++) {
+				assetNames[i] = DB_PATH + "/" + assetNames[i];
+			}
+			copyAssetsToDirectory(assetNames, dataDirectory);
+
+			DatabaseAccess.setDBPathName(dataDirectory.toString() + "/" + DatabaseAccess.dbName);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	public void copyAssetsToDirectory(String[] assets, File directory) throws IOException {
+		AssetManager assetManager = getAssets();
+
+		for (String asset : assets) {
+			String[] components = asset.split("/");
+			String copyPath = directory.toString() + "/" + components[components.length - 1];
+			char[] buffer = new char[1024];
+			int count;
+
+			File outFile = new File(copyPath);
+
+			if (!outFile.exists()) {
+				InputStreamReader in = new InputStreamReader(assetManager.open(asset));
+				FileWriter out = new FileWriter(outFile);
+
+				count = in.read(buffer);
+				while (count != -1) {
+					out.write(buffer, 0, count);
+					count = in.read(buffer);
+				}
+
+				out.close();
+				in.close();
+			}
 		}
 	}
 }
--- app/src/main/java/comp3350/plantr/presentation/MainActivity.java
+++ app/src/main/java/comp3350/plantr/presentation/MainActivity.java
@@ -1,8 +1,6 @@
 package comp3350.plantr.presentation;
 
 import android.app.Fragment;
-import android.content.Context;
-import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,17 +11,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import android.widget.Toast;
 
 import comp3350.plantr.R;
 import comp3350.plantr.business.DatabaseAccess;
 import comp3350.plantr.business.UserManager;
 import comp3350.plantr.business.exceptions.DatabaseCloseFailureException;
-import comp3350.plantr.business.exceptions.DatabaseStartFailureException;
 import comp3350.plantr.business.exceptions.UserLoginException;
 import comp3350.plantr.model.User;
 
@@ -34,9 +27,6 @@ public class MainActivity extends AppCompatActivity
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		try {
-			copyDatabaseToDevice();
-			DatabaseAccess.open();
-
 			setContentView(R.layout.activity_main);
 			Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 			setSupportActionBar(toolbar);
@@ -59,13 +49,8 @@ public class MainActivity extends AppCompatActivity
 
 			//set default fragment to garden layout
 			displayFragment(R.id.nav_garden_layout);
-
-			//TODO make exception more specific
-		} catch (DatabaseStartFailureException e) {
-			//TODO should make a toast to print msg
-			System.out.println("Database failed during open");
-			e.printStackTrace();
 		} catch (UserLoginException e) {
+			Toast.makeText(getApplicationContext(), R.string.login_user_login_failure, Toast.LENGTH_LONG).show();
 			e.printStackTrace();
 		}
 	}
@@ -77,8 +62,7 @@ public class MainActivity extends AppCompatActivity
 		try {
 			DatabaseAccess.close();
 		} catch (DatabaseCloseFailureException e) {
-			//TODO add better handling
-			System.out.println("Failure during database close.");
+			Toast.makeText(getApplicationContext(), R.string.main_database_close_failure, Toast.LENGTH_LONG).show();
 			e.printStackTrace();
 		}
 	}
@@ -122,51 +106,5 @@ public class MainActivity extends AppCompatActivity
 		drawer.closeDrawer(GravityCompat.START);
 	}
 
-	private void copyDatabaseToDevice() {
-		final String DB_PATH = "db";
 
-		String[] assetNames;
-		Context context = getApplicationContext();
-		File dataDirectory = context.getDir(DB_PATH, Context.MODE_PRIVATE);
-		AssetManager assetManager = getAssets();
-
-		try {
-			assetNames = assetManager.list(DB_PATH);
-			for (int i = 0; i < assetNames.length; i++) {
-				assetNames[i] = DB_PATH + "/" + assetNames[i];
-			}
-			copyAssetsToDirectory(assetNames, dataDirectory);
-
-			DatabaseAccess.setDBPathName(dataDirectory.toString() + "/" + DatabaseAccess.dbName);
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-	}
-
-	public void copyAssetsToDirectory(String[] assets, File directory) throws IOException {
-		AssetManager assetManager = getAssets();
-
-		for (String asset : assets) {
-			String[] components = asset.split("/");
-			String copyPath = directory.toString() + "/" + components[components.length - 1];
-			char[] buffer = new char[1024];
-			int count;
-
-			File outFile = new File(copyPath);
-
-			if (!outFile.exists()) {
-				InputStreamReader in = new InputStreamReader(assetManager.open(asset));
-				FileWriter out = new FileWriter(outFile);
-
-				count = in.read(buffer);
-				while (count != -1) {
-					out.write(buffer, 0, count);
-					count = in.read(buffer);
-				}
-
-				out.close();
-				in.close();
-			}
-		}
-	}
 }
